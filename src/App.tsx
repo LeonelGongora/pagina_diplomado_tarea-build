1d4657e50be98fa6c85f2d8c0c6bc9f7b4e18537
leonel2500 <strokehead@outlook.es>
2025-03-08 12:21:54 -0400

    Segundo commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import ListGroup from 'react-bootstrap/ListGroup';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className='contenido-grupo'>
+        <h3> Grupo YalaSoft </h3>
+
+        <div>
+          <ListGroup as="ol" numbered>
+            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+            <ListGroup.Item as="li">Cras justo odio</ListGroup.Item>
+          </ListGroup>
+        </div>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn moreadsadaasd
-      </p>
     </>
   )
 }
